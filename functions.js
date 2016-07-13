ed7f257e79c3791b3ea8e0e1cb8c93e779a04261
Mark Dickinson <monoposon@comcast.net>
2016-07-13 12:33:18 -0600

    finished #2

--- functions.js
+++ functions.js
@@ -1,3 +1,38 @@
+//  #1.
+
+var people = {
+  Chuck: 22,
+  Brian: 46
+};
+
+function oldestPerson(obj){
+  var oldest = 0;
+  for (var keys in obj) {
+    if (oldest < obj[keys]){
+      oldest = obj[keys];
+    };
+  };
+  return oldest
+};
+
+console.log(oldestPerson(people));
+
+//  #2.
+
+function longestWord(str){
+  var longest = "";
+  var strArr = str.split(" ");
+  for (var i = 0; i < strArr.length; i++){
+    if (strArr[i].length > longest.length){
+      longest = strArr[i];
+    };
+  };
+  return longest;
+}
+
+console.log(longestWord('This is a very long sentence with a long word sentence!'));
+
+//  #5. Bonus!
 
 function isPalindrome(str){
   var str = str.toLowerCase();
